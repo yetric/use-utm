75540eb7595a41ce04305c8b626ab6aa60661edc
Mattias Hising <mattias.hising@caspeco.se>
2023-12-27 13:41:06 +0100

    First

---
+++ src/useUtm.ts
@@ -0,0 +1,24 @@
+type MarketingParams = {
+    source: string | null;
+    medium: string | null;
+    campaign: string | null;
+    term: string | null;
+    content: string | null;
+}
+
+export const useUtm = (): MarketingParams => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const utmSource = urlParams.get("utm_source");
+    const utmMedium = urlParams.get("utm_medium");
+    const utmCampaign = urlParams.get("utm_campaign");
+    const utmTerm = urlParams.get("utm_term");
+    const utmContent = urlParams.get("utm_content");
+
+    return {
+        source: utmSource,
+        medium: utmMedium,
+        campaign: utmCampaign,
+        term: utmTerm,
+        content: utmContent
+    };
+}
